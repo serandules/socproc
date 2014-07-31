a7bfda68fad18c09689920831559517b381ab85e
ruchiraw <ruchira.wageesha@gmail.com>
2014-08-01 00:02:17 +0530

    initial version

---
+++ index.js
@@ -0,0 +1,19 @@
+var Server = require('./lib/server');
+
+var socprocs = [];
+
+var connect = function (socket) {
+
+};
+
+var disconnect = function () {
+
+};
+
+var ChildProcess = function () {
+
+};
+
+module.exports = function (id, io, options) {
+    return new Server(id, io, options);
+};
